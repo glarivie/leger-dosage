f1b0af979d18fd1b4ad46a58a877479a541ebe07
Guillaume L <guillaume@skillup.co>
2021-05-05 21:47:47 +0200

    [ADD] miniature img width

--- src/components/CardMiniature/CardMiniature.tsx
+++ src/components/CardMiniature/CardMiniature.tsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useMedia } from "react-use";
 import Link from "next/link";
 
 import type { Project } from "types";
 
 import styles from "./CardMiniature.module.scss";
 
-const CardMiniature = ({ category, title, miniature, excerpt, slug }: Project) => (
-  <Link href="/[slug]" as={`/${slug}`}>
-    <a className={styles.CardMiniature}>
-      <div
-        className={styles.illustration}
-        style={{ backgroundImage: `url(${miniature.fields.file.url})` }}
-      />
-      <div className={styles.content}>
-        <span className={styles.category}>{category}</span>
-        <h2 className={styles.title}>{title}</h2>
-        <p className={styles.excerpt}>{excerpt}</p>
-      </div>
-    </a>
-  </Link>
-);
+const CardMiniature = ({ category, title, miniature, excerpt, slug }: Project) => {
+  const isRetina = useMedia("(min-resolution: 2dppx)");
+  const width = useMemo(() => (isRetina ? 960 : 480), [isRetina]);
+
+  return (
+    <Link href="/[slug]" as={`/${slug}`}>
+      <a className={styles.CardMiniature}>
+        <div
+          className={styles.illustration}
+          style={{ backgroundImage: `url(https:${miniature.fields.file.url}?w=${width})` }}
+        />
+        <div className={styles.content}>
+          <span className={styles.category}>{category}</span>
+          <h2 className={styles.title}>{title}</h2>
+          <p className={styles.excerpt}>{excerpt}</p>
+        </div>
+      </a>
+    </Link>
+  );
+};
 
 export default CardMiniature;
